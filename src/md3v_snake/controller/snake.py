a245c69bb2ae52edc5293580888eca6b4b738141
Mitchell Devlin <mitchell.r.devlin@gmail.com>
2022-10-16 13:39:27 -0400

    Fixing MVC breakup of gameboard for snake game. Refactoring to have all pygame screen touching code in the respective view classes.

--- src/md3v_snake/controller/snake.py
+++ src/md3v_snake/controller/snake.py
@@ -5,6 +5,8 @@ from md3v_snake.model.assets import Assets
 from md3v_snake.config.load_config import load as conf_load
 import md3v_snake.consts as consts
 import pygame
+from md3v_snake.model.player import Player
+import random
 
 class Snake:
     def __init__( self, config_file ):
@@ -26,17 +28,127 @@ class Snake:
 
         self.start_up_menu = StartupMenu( self.config, assets, screen )
         self.game_board = GameBoard( self.config, assets, screen )
+        self.start_up = False
+        self.running = False
+
+        self.player_speed = self.config[consts.PLAYER_SPEED]
 
     def run( self ):    
         self.start_up_menu.show_startup_menu()
-        while True:
+        self.start_up = True
+
+        p1 = Player()
+        players = [p1]
+        objective_pos = [random.randrange(0, 1000, 20), random.randrange(0, 600, 20)]
+        # default change
+        change_in_position = (0, 20)
+
+        while self.start_up:
             mouse = pygame.mouse.get_pos()
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONUP:
                     if self.start_up_menu.check_start_button( mouse ):
-                        self.game_board.start()
+                        self.game_board.show_game_board( objective_pos, players)
+                        self.start_up = False
                     if self.start_up_menu.check_options_button( mouse ):
-                        return
+                        # TODO: Implement an options menu for changing configurable options, etc...
+                        self.start_up = False
             # FIXME: Player speed is the same as time delay for updating screen?
             pygame.time.delay(self.speed)
+        
+        self.running = True
+        while self.running:
+            # Entire board game is full
+            # Todo: Locked into screen size
+            if len(players) == 80:
+                self.running = False
+                self.youWin()
 
+            for event in pygame.event.get():
+                # Quit game when exit button is pressed
+                if event.type == pygame.QUIT:
+                    notOver = False
+                    self.game_over()
+
+                # Alter direction of players move
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        if change_in_position != (0, 20):
+                            change_in_position = (0, -20)
+                    elif event.key == pygame.K_DOWN:
+                        if change_in_position != (0, -20):
+                            change_in_position = (0, 20)
+                    elif event.key == pygame.K_RIGHT:
+                        if change_in_position != (-20, 0):
+                            change_in_position = (20, 0)
+                    elif event.key == pygame.K_LEFT:
+                        if change_in_position != (20, 0):
+                            change_in_position = (-20, 0)
+
+            # record last position and change player's current position 
+            players[0].last_position = players[0].position
+            players[0].position = list(map(sum, zip(players[0].position, change_in_position)))
+
+            self.checkScreenLimits( players[0] )
+
+            # objective player reached
+            if players[0].position == objective_pos:
+                objective_pos = [random.randrange(0, 1000, 20), random.randrange(0, 600, 20)]
+                while True:
+                    elem_in_list = False
+                    for i in range(0, len(players)):
+                        if players[i].position == objective_pos:
+                            objective_pos = [random.randrange(0, 1000, 20), random.randrange(0, 600, 20)]
+                            elem_in_list = True
+                            break
+                        elem_in_list = False
+                    
+                    if elem_in_list == False:
+                        break
+
+                p2 = Player()
+                players.append(p2)
+                if self.player_speed > 75:
+                    self.player_speed -= 4
+
+            for i in range(1, len(players)):
+                players[i].last_position = players[i].position
+                players[i].position = players[i-1].last_position
+
+            if self.inList(players, players[0]):
+                notOver = False
+                self.game_over()
+
+            self.game_board.update_player_pos( objective_pos, players )
+            # FIXME: Player speed is the same as time delay for updating screen?
+            self.game_board.update_player_speed( self.player_speed )
+
+    def checkScreenLimits( self, player ):
+        # Get the screen dimensions which act as boundaries for
+        # the snake "player"
+        screen_rect = self.game_board.get_screen_rect()
+        screen_x_boundary = screen_rect[2]
+        screen_y_boundary = screen_rect[3]
+
+        if player.position[consts.X_POS_IDX] >= screen_x_boundary:
+            player.position[consts.X_POS_IDX] = 0
+        elif player.position[consts.X_POS_IDX] < 0:
+            player.position[consts.X_POS_IDX] = screen_x_boundary
+
+        if player.position[consts.Y_POS_IDX] >= screen_y_boundary:
+            player.position[consts.Y_POS_IDX] = 0
+        elif player.position[consts.Y_POS_IDX] < 0:
+            player.position[consts.Y_POS_IDX] = screen_y_boundary
+    
+    def inList(self, a, x):
+        for i in range(1, len(a)):
+            if a[i].position == x.position:
+                    return True
+        return False
+
+    def game_over(self):
+        pygame.quit()
+
+    def youWin(self):
+        print("you win!")
+        pygame.quit()
--- src/md3v_snake/view/game_board.py
+++ src/md3v_snake/view/game_board.py
@@ -1,127 +1,27 @@
 import pygame
-import random
 import md3v_snake.consts as consts
-from md3v_snake.model.player import Player
 
 class GameBoard:
     def __init__( self, config, assets, screen ):
         self.assets = assets
         self.screen = screen
-        self.player_speed = config[ consts.PLAYER_SPEED ]
-        self.running = False
 
-    def start( self ):
-
-        self.running = True
-        p1 = Player()
-        players = [p1]
-        objective_pos = [random.randrange(0, 1000, 20), random.randrange(0, 600, 20)]
-                
+    def show_game_board( self, objective_pos, players ):
         self.screen.blit(self.assets.get_background_asset(), (0, 0))
         self.screen.blit(self.assets.get_image_asset(), players[0].position)
         self.screen.blit(self.assets.get_image_asset(), objective_pos)
         pygame.display.update()
 
-        # default change
-        change_in_position = (0, 20)
+    def update_player_pos( self, objective_pos, players ):
+        for player in players:
+            self.screen.blit(self.assets.get_clear_asset(), player.last_position)
+            self.screen.blit(self.assets.get_image_asset(), player.position)
 
-        while self.running:
-            # Entire board game is full
-            # Todo: Locked into screen size
-            if len(players) == 80:
-                self.running = False
-                self.youWin()
-            
-            self.screen.blit(self.assets.get_clear_asset(), players[0].position)
-            for event in pygame.event.get():
-                # Quit game when exit button is pressed
-                if event.type == pygame.QUIT:
-                    notOver = False
-                    self.game_over()
+        self.screen.blit(self.assets.get_image_asset(), objective_pos)
+        pygame.display.update()
 
-                # Alter direction of players move
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        if change_in_position != (0, 20):
-                            change_in_position = (0, -20)
-                    elif event.key == pygame.K_DOWN:
-                        if change_in_position != (0, -20):
-                            change_in_position = (0, 20)
-                    elif event.key == pygame.K_RIGHT:
-                        if change_in_position != (-20, 0):
-                            change_in_position = (20, 0)
-                    elif event.key == pygame.K_LEFT:
-                        if change_in_position != (20, 0):
-                            change_in_position = (-20, 0)
+    def update_player_speed( self, player_speed ):
+        pygame.time.delay(player_speed)
 
-            # record last position and change player's current position 
-            players[0].last_position = players[0].position
-            players[0].position = list(map(sum, zip(players[0].position, change_in_position)))
-
-            self.checkScreenLimits( players[0] )
-
-            # objective player reached
-            if players[0].position == objective_pos:
-                self.screen.blit(self.assets.get_clear_asset(), objective_pos)
-                objective_pos = [random.randrange(0, 1000, 20), random.randrange(0, 600, 20)]
-                while True:
-                    elem_in_list = False
-                    for i in range(0, len(players)):
-                        if players[i].position == objective_pos:
-                            objective_pos = [random.randrange(0, 1000, 20), random.randrange(0, 600, 20)]
-                            elem_in_list = True
-                            break
-                        elem_in_list = False
-                    
-                    if elem_in_list == False:
-                        break
-
-                p2 = Player()
-                players.append(p2)
-                if self.player_speed > 75:
-                    self.player_speed -= 4
-
-            for i in range(1, len(players)):
-                players[i].last_position = players[i].position
-                self.screen.blit(self.assets.get_clear_asset(), players[i].position)
-                players[i].position = players[i-1].last_position
-                self.screen.blit(self.assets.get_image_asset(), players[i].position)
-
-            if self.inList(players, players[0]):
-                notOver = False
-                self.game_over()
-
-            self.screen.blit(self.assets.get_image_asset(), players[0].position)
-            self.screen.blit(self.assets.get_image_asset(), objective_pos)
-            pygame.display.update()
-            # FIXME: Player speed is the same as time delay for updating screen?
-            pygame.time.delay(self.player_speed)
-
-    def checkScreenLimits( self, player ):
-        # Get the screen dimensions which act as boundaries for
-        # the snake "player"
-        screen_x_boundary = self.screen.get_rect()[2]
-        screen_y_boundary = self.screen.get_rect()[3]
-
-        if player.position[consts.X_POS_IDX] >= screen_x_boundary:
-            player.position[consts.X_POS_IDX] = 0
-        elif player.position[consts.X_POS_IDX] < 0:
-            player.position[consts.X_POS_IDX] = screen_x_boundary
-
-        if player.position[consts.Y_POS_IDX] >= screen_y_boundary:
-            player.position[consts.Y_POS_IDX] = 0
-        elif player.position[consts.Y_POS_IDX] < 0:
-            player.position[consts.Y_POS_IDX] = screen_y_boundary
-    
-    def inList(self, a, x):
-        for i in range(1, len(a)):
-            if a[i].position == x.position:
-                    return True
-        return False
-
-    def game_over(self):
-        pygame.quit()
-
-    def youWin(self):
-        print("you win!")
-        pygame.quit()
+    def get_screen_rect( self ):
+        return self.screen.get_rect()
